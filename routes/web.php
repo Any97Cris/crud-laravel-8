4b88a6c6f510e5a900975dc8ce701bdafa666448
crisciany.silva <criscianysilva1997@gmail.com>
2023-11-19 11:20:28 -0400

    add middleware e correção error com estilo vite

--- routes/web.php
+++ routes/web.php
@@ -18,15 +18,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::middleware(['auth'])->group(function () {
+    Route::any('/posts/search', [PostController::class, 'search'])->name('post.search');
+    Route::get('/posts/create', [PostController::class, 'create'])->name('post.create');
+    Route::put('/posts/update/{id}',[PostController::class, 'update'])->name('post.update');
+    Route::get('/posts/edit/{id}',[PostController::class, 'edit'])->name('post.edit');
+    Route::delete('/posts/{id}', [PostController::class, 'destroy'])->name('post.delete');
+    Route::get('posts/{id}', [PostController::class, 'show'])->name('post.show');
+    Route::get('/posts', [PostController::class, 'index'])->name('post.index');
+    Route::post('/posts/store', [PostController::class, 'store'])->name('post.store');
+});
 
-Route::any('/posts/search', [PostController::class, 'search'])->name('post.search');
-Route::get('/posts/create', [PostController::class, 'create'])->name('post.create');
-Route::put('/posts/update/{id}',[PostController::class, 'update'])->name('post.update');
-Route::get('/posts/edit/{id}',[PostController::class, 'edit'])->name('post.edit');
-Route::delete('/posts/{id}', [PostController::class, 'destroy'])->name('post.delete');
-Route::get('posts/{id}', [PostController::class, 'show'])->name('post.show');
-Route::get('/posts', [PostController::class, 'index'])->name('post.index');
-Route::post('/posts/store', [PostController::class, 'store'])->name('post.store');
 
 
 Route::get('/dashboard', function () {
